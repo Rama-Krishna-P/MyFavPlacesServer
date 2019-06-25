a59fef8ee8901bbc3ff5e6f4c66331a7acb79dc1
P Rama-Krishna <rama-krishna.p@alstomgroup.com>
2019-06-25 23:25:22 +0530

    Removed jsonwebtoken and bcrypt

--- app/controllers/signInController.ts
+++ app/controllers/signInController.ts
@@ -1,8 +1,8 @@
 import { ControllerBase } from "./controller-base";
 import { Controller } from "../interfaces/controller";
 import { Application, Request, Response, NextFunction } from "express";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+// import bcrypt from 'bcrypt';
+//import jwt from 'jsonwebtoken';
 
 export class SignInController extends ControllerBase implements Controller {
     constructor() {
@@ -15,32 +15,32 @@ export class SignInController extends ControllerBase implements Controller {
 
     private signIn(req: Request, res: Response, nextFunction: NextFunction) {
         // password: Rama
-        if (req.body.userName === 'rkp' && req.body.password) {
-            bcrypt.compare(req.body.password, '$2b$10$kvv9iZZexA4V8McTOgUs9uZvinonqm4expcoxy66bJ32Q4x.Vh3su', (err, result) => {
-                if (result) {
-                    let key: string = process.env.JSON_KEY ? process.env.JSON_KEY.valueOf() : "secret";
-                    let expiresIn = process.env.JWT_TIMEOUT ? process.env.JWT_TIMEOUT.valueOf() : 180;
+        // if (req.body.userName === 'rkp' && req.body.password) {
+        //     bcrypt.compare(req.body.password, '$2b$10$kvv9iZZexA4V8McTOgUs9uZvinonqm4expcoxy66bJ32Q4x.Vh3su', (err, result) => {
+        //         if (result) {
+        //             let key: string = process.env.JSON_KEY ? process.env.JSON_KEY.valueOf() : "secret";
+        //             let expiresIn = process.env.JWT_TIMEOUT ? process.env.JWT_TIMEOUT.valueOf() : 180;
 
-                    let token = jwt.sign({ userName: req.body.userName, expires: true }, key, {
-                        expiresIn: expiresIn
-                    })
-                    res.setHeader('authorization', `Bearer ${token}`);
-                    res.status(200).json({
-                        userName: req.body.userName,
-                        token: token,
-                    });
-                }
-                else {
-                    res.status(401).json({
-                        message: 'Auth failed'
-                    })
-                }
-            })
-        }
-        else {
-            res.status(401).json({
-                message: 'Auth failed'
-            })
-        }
+        //             let token = jwt.sign({ userName: req.body.userName, expires: true }, key, {
+        //                 expiresIn: expiresIn
+        //             })
+        //             res.setHeader('authorization', `Bearer ${token}`);
+        //             res.status(200).json({
+        //                 userName: req.body.userName,
+        //                 token: token,
+        //             });
+        //         }
+        //         else {
+        //             res.status(401).json({
+        //                 message: 'Auth failed'
+        //             })
+        //         }
+        //     })
+        // }
+        // else {
+        //     res.status(401).json({
+        //         message: 'Auth failed'
+        //     })
+        // }
     }
 }
--- app/middleware/jwtAuthorizer.ts
+++ app/middleware/jwtAuthorizer.ts
@@ -1,32 +1,33 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
 export function Authorize(req: Request, res: Response, next: NextFunction) {
-    let token = req.headers.authorization ? req.headers.authorization.valueOf().split(' ')[1] : undefined;
-    let key = process.env.JSON_KEY ? process.env.JSON_KEY.valueOf() : 'secret';
-    let expiresIn = process.env.JWT_TIMEOUT ? process.env.JWT_TIMEOUT.valueOf() : 180;
+    // let token = req.headers.authorization ? req.headers.authorization.valueOf().split(' ')[1] : undefined;
+    // let key = process.env.JSON_KEY ? process.env.JSON_KEY.valueOf() : 'secret';
+    // let expiresIn = process.env.JWT_TIMEOUT ? process.env.JWT_TIMEOUT.valueOf() : 180;
 
-    if (token && key) {
-        try {
-            const decoded = JSON.parse(JSON.stringify(jwt.verify(token, key).valueOf()));
-            res.locals.userName = decoded.userName;
+    // if (token && key) {
+    //     try {
+    //         const decoded = JSON.parse(JSON.stringify(jwt.verify(token, key).valueOf()));
+    //         res.locals.userName = decoded.userName;
             
-            if (decoded.expires) {
-                let token = jwt.sign({ userName: decoded.userName, expires: true }, key, {
-                    expiresIn: expiresIn });
-                res.setHeader('authorization', `Bearer ${token}`);
-            }
+    //         if (decoded.expires) {
+    //             let token = jwt.sign({ userName: decoded.userName, expires: true }, key, {
+    //                 expiresIn: expiresIn });
+    //             res.setHeader('authorization', `Bearer ${token}`);
+    //         }
 
-            next();
-        } catch (error) {
-            res.status(401).json({
-                message: error
-            })
-        }
-    }
-    else {
-        res.status(401).json({
-            message: 'Auth failed'
-        })
-    }
+    //         next();
+    //     } catch (error) {
+    //         res.status(401).json({
+    //             message: error
+    //         })
+    //     }
+    // }
+    // else {
+    //     res.status(401).json({
+    //         message: 'Auth failed'
+    //     })
+    // }
+    next();
 }
