9eacab61aabfbed059861c1dcb0c3dab532e073e
P Rama-Krishna <rama-krishna.p@alstomgroup.com>
2019-06-25 09:09:33 +0530

    deleted unwanted files

--- app/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Application_1 = require("./Application");
-var app = new Application_1.Application();
-app.startApp();
